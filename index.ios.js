10cda993fb93bacefb2901f5ae6f44459aba65c8
nshung <brangnu@gmail.com>
2016-06-24 12:03:32 +0200

    Removed redendency in code

--- index.ios.js
+++ index.ios.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 
 import React, { Component } from 'react';
 import {
@@ -16,108 +10,35 @@ import {
   View
 } from 'react-native';
 
+import {Router, routerReducer, Route, Container, Animations, Schema} from 'react-native-redux-router';
+import Login  from './login.ios.js';
+import Home from './home.ios.js';
 
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
+let store = createStore(combineReducers({routerReducer}));
 
-class puk extends Component {
-
-  constructor(props) {
-      super(props);
-      this.state = { };
-  }
-
-
-  _onPressButton(username, password){
-    if (username == 'admin', password == 'secret'){
-      console.log("you are logged in")
-    }else {
-      console.log('wrong.please try again.')
-    }
-  }
-
-  render() {
+class App extends Component {
+  render(){
     return (
-      <View style={styles.container}>
-        <Image style={styles.appicon} source={require('./img/appicon.png')} />
-        
-        <Text style={styles.welcome}>
-          Welcome to PeopleUKnow!
-        </Text>
-
-
-        <TextInput
-            autoCapitalize={'none'}
-            style={styles.username}
-            onChangeText={(username) => this.setState({username})}
-            value = {this.state.username}
-            placeholder={'Username'}/>
-
-        <TextInput
-            autoCapitalize={'none'}
-            style={styles.password}
-            onChangeText={(password) => this.setState({password})}
-            value={this.state.password}
-            placeholder={'Password'}/>
-
-        <TouchableHighlight style={{ marginTop : 10}} onPress={() => this._onPressButton(this.state.username, this.state.password)}>
-              <Image
-                style={styles.button}
-                source={require('./img/login.png')}/>
-        </TouchableHighlight>
-
+      <View style={{flex:1}}>
+        <View style={{position:'absolute',left:0,right:0,top:0,bottom:0,backgroundColor:'#F5FCFF'}}/>
+        <Router>
+          <Route name="login" initial={true} component={Login} title="Login"/>
+          <Route name="home"  component={Home} title="Home"/>
+        </Router>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    //justifyContent: 'center',
-    alignItems: 'center',
-    marginTop : 35,
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  appicon : {
-    marginTop : 10,
-    width : 50,
-    height : 50
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  username:{
-    borderWidth : 2,
-    height : 40,
-    width : 200,
-    marginLeft : 60,
-    marginRight : 50,
-    paddingLeft : 10,
-    marginTop : 100,
-    flexDirection:'row',
-    alignItems:'center',
-    justifyContent:'center'
-  },
-
-  password:{
-    borderWidth : 2,
-    height : 40,
-    width : 200,
-    marginLeft : 60,
-    marginRight : 50,
-    paddingLeft : 10,
-    marginTop : 10,
-    flexDirection:'row',
-    alignItems:'center',
-    justifyContent:'center'
+class puk extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
   }
-});
-
+}
 AppRegistry.registerComponent('puk', () => puk);
